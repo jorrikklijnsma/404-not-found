423e33f499068f0c6a88fc58ff59cb49351fc4d0
jorrikklijnsma <jorrikklijnsma@gmail.com>
2022-01-29 14:47:04 +0100

    Add Responsiveness

--- src/Error404.js
+++ src/Error404.js
@@ -10,12 +10,12 @@ if (process.env.NODE_ENV !== 'development') {
 
 export function Error404() {
     return (
-        <div className="h-screen w-screen flex justify-center items-center bg-gray-900 text-white font-mono text-center">
-            <div className="h-1/3 flex flex-col justify-between">
-                <h1 className="text-7xl">
+        <div className="absolute inset-0 flex justify-center items-center bg-gray-900 text-white font-mono text-center">
+            <div className="lg:h-1/3 flex flex-col justify-between">
+                <h1 className="text-xl sm:text-3xl xl:text-7xl mb-12">
                     <strong>404</strong> - Page Not Found<sup>*</sup>
                 </h1>
-                <div>
+                <div className="transform scale-75 md:scale-100">
                     <p className="mb-8">
                         Idea stolen from <a href="https://twitter.com/renevanm" target="blank" className="font-bold">@renevanm</a>
                     </p>
@@ -23,7 +23,7 @@ export function Error404() {
                         Created by <a href="https://github.com/jorrikklijnsma" target="blank" className="underline">this guy</a>
                     </p>
                 </div>
-                <p className="fixed bottom-4 right-4 text-gray-500 text-xs">
+                <p className="absolute bottom-4 right-4 text-gray-500 text-xs">
                     <sup>*</sup>And is the answer to the URL
                 </p>
             </div>
