2bbe6cca271e86c2c788a53f7369672929f8b1f1
jorrikklijnsma <jorrikklijnsma@gmail.com>
2022-01-29 22:07:19 +0100

    Update manifest

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Error404 } from './Error404.js';
 
+const link = document.createElement("link");
+link.rel = "manifest";    
+link.setAttribute('href', `./${process.env.REACT_APP_LANG}Manifest.json`)
+document.head.appendChild(link);
+
 ReactDOM.render(
   <React.StrictMode>
     <Error404 />
