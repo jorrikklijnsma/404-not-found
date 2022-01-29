96e7626b03d998fd3828d7718066ee46fc8bdced
jorrikklijnsma <jorrikklijnsma@gmail.com>
2022-01-29 22:30:31 +0100

    fix site name en meta description

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ link.rel = "manifest";
 link.setAttribute('href', `./${process.env.REACT_APP_LANG}Manifest.json`)
 document.head.appendChild(link);
 
+const meta = document.createElement("meta");
+const title = document.createElement("title");
+meta.setAttribute('name', `description`)
+if (process.env.REACT_APP_LANG === 'nl') {
+  meta.setAttribute('content', 'Wat is de wortel van 163216?')
+  title.innerText = '404 - Pagina Niet Gevonden';
+} else {
+  meta.setAttribute('content', 'The Root of 163216')
+  title.innerText = '404 - Page Not Found';
+}  
+document.head.appendChild(meta);
+document.head.appendChild(title);
+
 ReactDOM.render(
   <React.StrictMode>
     <Error404 />
